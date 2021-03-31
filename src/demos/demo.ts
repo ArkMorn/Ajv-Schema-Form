dd24d662a9ce704bed92bdadd1d90ded448ac922
M <584706394@qq.com>
2021-03-31 19:05:49 +0800

    schemaForm

--- src/demos/demo.ts
+++ src/demos/demo.ts
@@ -1,4 +1,4 @@
-import PasswordWidget from '../components/PasswordWiget'
+// import PasswordWidget from '../components/PasswordWiget'
 
 export default {
   name: 'Demo',
@@ -23,25 +23,25 @@ export default {
       },
     },
   },
-  async customValidate(data: any, errors: any) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        if (data.pass1 !== data.pass2) {
-          errors.pass2.addError('密码必须相同')
-        }
-        resolve()
-      }, 2000)
-    })
-  },
-  uiSchema: {
-    properties: {
-      pass1: {
-        widget: PasswordWidget,
-      },
-      pass2: {
-        color: 'red',
-      },
-    },
-  },
+  // async customValidate(data: any, errors: any) {
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       if (data.pass1 !== data.pass2) {
+  //         errors.pass2.addError('密码必须相同')
+  //       }
+  //       resolve()
+  //     }, 2000)
+  //   })
+  // },
+  // uiSchema: {
+  //   properties: {
+  //     pass1: {
+  //       widget: PasswordWidget,
+  //     },
+  //     pass2: {
+  //       color: 'red',
+  //     },
+  //   },
+  // },
   default: 1,
 }
